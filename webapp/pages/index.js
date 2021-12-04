90fb5ff4909fa96f4c9087d65caf48b1f6c57272
Choudhary Abdullah <magnetonx@gmail.com>
2021-12-04 23:02:11 +0530

    Fixed style

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,49 +1,49 @@
-import Head from 'next/head';
-import axios from 'axios';
-import {saveAs} from 'file-saver';
-import JSZip from 'jszip';
-import {useState} from 'react';
+import Head from 'next/head'
+import axios from 'axios'
+import { saveAs } from 'file-saver'
+import JSZip from 'jszip'
+import { useState } from 'react'
 
 export default function Home() {
-	let [isActive, setActive] = useState(false);
+	let [isActive, setActive] = useState(false)
 	const fetchMedia = async (event) => {
-		setActive(true);
-		event.preventDefault();
-		let postURL = event.target.postURL.value;
+		setActive(true)
+		event.preventDefault()
+		let postURL = event.target.postURL.value
 		await axios
 			.get('https://instaasnap.app/webapi/?postURL=' + postURL)
 			.then(async (response) => {
 				if (response.data.mediaList.length === 1) {
-					let imageBlob = await fetch(response.data.mediaList[0]).then((response) => response.blob());
+					let imageBlob = await fetch(response.data.mediaList[0]).then((response) => response.blob())
 					if (response.data.mediaList[0].search('.mp4') === -1) {
-						saveAs(imageBlob, Math.floor(Math.random() * 999999999999999999999) + '.jpg');
-						setActive(false);
+						saveAs(imageBlob, Math.floor(Math.random() * 999999999999999999999) + '.jpg')
+						setActive(false)
 					} else {
-						saveAs(imageBlob, Math.floor(Math.random() * 999999999999999999999) + '.mp4');
-						setActive(false);
+						saveAs(imageBlob, Math.floor(Math.random() * 999999999999999999999) + '.mp4')
+						setActive(false)
 					}
 				} else if (response.data.mediaList.length > 1) {
-					let zip = new JSZip();
-					let folder = zip.folder('collection');
+					let zip = new JSZip()
+					let folder = zip.folder('collection')
 					for (let mediaFile of response.data.mediaList) {
-						let mediaName = null;
+						let mediaName = null
 						if (mediaFile.search('.mp4') === -1) {
-							mediaName = Math.floor(Math.random() * 999999999999999999999) + '.jpg';
-						} else mediaName = Math.floor(Math.random() * 999999999999999999999) + '.mp4';
+							mediaName = Math.floor(Math.random() * 999999999999999999999) + '.jpg'
+						} else mediaName = Math.floor(Math.random() * 999999999999999999999) + '.mp4'
 
-						let imageBlob = await fetch(mediaFile).then((response) => response.blob());
-						folder.file(mediaName, imageBlob);
+						let imageBlob = await fetch(mediaFile).then((response) => response.blob())
+						folder.file(mediaName, imageBlob)
 					}
-					folder.generateAsync({type: 'blob'}).then((content) => {
-						saveAs(content, Math.floor(Math.random() * 999999999999999999999) + '.zip');
-						setActive(false);
-					});
+					folder.generateAsync({ type: 'blob' }).then((content) => {
+						saveAs(content, Math.floor(Math.random() * 999999999999999999999) + '.zip')
+						setActive(false)
+					})
 				}
 			})
 			.catch((error) => {
-				console.log(error);
-			});
-	};
+				console.log(error)
+			})
+	}
 
 	return (
 		<div className='mainDiv'>
@@ -71,5 +71,5 @@ export default function Home() {
 				</div>
 			</form>
 		</div>
-	);
+	)
 }

e9eef365b8c676f4750a7d36076ad6d7bffec416
Choudhary Abdullah <magnetonx@gmail.com>
2021-12-18 20:20:19 +0530

    Linting

--- webapp/pages/privacy.js
+++ webapp/pages/privacy.js
@@ -11,7 +11,7 @@ export default function Home() {
         <h1 className='heading'>Privacy Policy</h1>
         <p className='paragraph'>1. At InstaaSnap, we care about your privacy.</p>
         <p className='paragraph'>2. We believe in complete transparency.</p>
-        <p className='paragraph'>3. We're committed to being upfront about our privacy practices.</p>
+        <p className='paragraph'>3. We&apos;re committed to being upfront about our privacy practices.</p>
         <p className='paragraph'>4. We do not collect, store or save any information at all.</p>
         <p className='paragraph'>5. We do not use cookies.</p>
         <p className='paragraph'>6. We do not save any logs.</p>
